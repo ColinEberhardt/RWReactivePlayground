b67840af9d67bd635b9c6e7535c92bf8343ff615
Colin Eberhardt <ceberhardt@scottlogic.co.uk>
2014-01-08 21:09:55 +0000

    Sign in now performs the required segue

--- RWReactivePlayground/RWViewController.m
+++ RWReactivePlayground/RWViewController.m
@@ -72,10 +72,13 @@
     flattenMap:^id(id x) {
       return [self signInSignal];
     }]
-    subscribeNext:^(id x) {
-      NSLog(@"Sign in result: %@", x);
+    subscribeNext:^(NSNumber *signedIn) {
+      BOOL success = [signedIn boolValue];
+      self.signInFailureText.hidden = success;
+      if (success) {
+        [self performSegueWithIdentifier:@"signInSuccess" sender:self];
+      }
     }];
-
 }
 
 - (BOOL)isValidUsername:(NSString *)username {
